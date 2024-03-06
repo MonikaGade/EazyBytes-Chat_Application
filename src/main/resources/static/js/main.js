6a7c7e50981c5c9b424f81200dd57e5b6ee72971
MonikaGade <mounika10901@gmail.com>
2024-03-06 22:04:24 +0530

    updated chat application

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,13 +1,13 @@
 'use strict';
-
+console.log("hello")
 const usernamePage = document.querySelector('#username-page');
 const chatPage = document.querySelector('#chat-page');
 const usernameForm = document.querySelector('#usernameForm');
-const messageForm = document.querySelector('#messageForm');
+const messageForm =document.querySelector('#messageForm');
 const messageInput = document.querySelector('#message');
 const connectingElement = document.querySelector('.connecting');
 const chatArea = document.querySelector('#chat-messages');
-const logout = document.querySelector('#logout');
+const logout =document.querySelector('#logout');
 
 let stompClient = null;
 let nickname = null;
